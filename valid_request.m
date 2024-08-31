90b3bc76e5c898dd4684ad0aa676c26f779eab1f
Tri Dung Nguyen <140864509+dungTnguyen05@users.noreply.github.com>
2024-08-31 19:40:48 +0930

    Delete valid_request.m

--- valid_request.m
+++
@@ -1,8 +0,0 @@
-% function to check if users input valid request (yes/no)
-function [valid] = valid_request(request)
-    if request == "yes" || request == "no"
-        valid = 1;
-    else
-        valid = 0;
-    end
-end
