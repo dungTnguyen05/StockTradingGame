e8917332675b7dc856eab8d4fc7f4069c135066a
Tri Dung Nguyen <140864509+dungTnguyen05@users.noreply.github.com>
2024-08-31 19:17:21 +0930

    Delete

--- test_display_stock_owned.m
+++
@@ -1,7 +0,0 @@
-% test uses of display_stock_owned() function
-stock_owned = [100, 200, 300, 200, 100];
-display_stock_owned(stock_owned);
-% => as expected
-stock_owned = [100, 200, 300, 400, 500];
-display_stock_owned(stock_owned);
-% => as expected
