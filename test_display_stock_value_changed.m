8fe6781a288313ab1a86dc8f8e30ca388fa0be57
Tri Dung Nguyen <140864509+dungTnguyen05@users.noreply.github.com>
2024-08-31 19:17:48 +0930

    Delete test_display_stock_value_changed.m

--- test_display_stock_value_changed.m
+++
@@ -1,8 +0,0 @@
-% test uses of display_stock_value_changed() function
-A = [100, 100, 100, 100, 100; 110, 120, 130, 140, 150; 200, 400, 600, 800, 1000];
-display_stock_value_changed(A, 1);
-% => as expected
-display_stock_value_changed(A, 2);
-% => as expected
-display_stock_value_changed(A, 3);
-% => as expected
