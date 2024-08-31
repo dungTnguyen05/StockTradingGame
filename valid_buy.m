44f4952b211b55f448d2d51b94328099ad3a4ff6
Tri Dung Nguyen <140864509+dungTnguyen05@users.noreply.github.com>
2024-08-31 19:40:56 +0930

    Delete valid_buy.m

--- valid_buy.m
+++
@@ -1,8 +0,0 @@
-% function to check if users input valid amount of stock to buy
-function [valid] = valid_buy(money_left, amount_stock, value_stock)
-    if isnumeric(amount_stock) && money_left >= amount_stock * value_stock && amount_stock >= 0
-        valid = 1;
-    else
-        valid = 0;
-    end
-end
